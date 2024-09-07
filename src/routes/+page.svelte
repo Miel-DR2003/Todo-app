d1f16a2516bfae8d3e7c4856a552b559a10175fb
Miel-DR2003 <mieldr1@gmail.com>
2024-09-07 21:15:09 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <link
+        rel="stylesheet"
+        href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css"
+    />
+</svelte:head>
+
+<!-- add css with tailwind -->
+<!-- margin:0 auto; padding: 20px; width:700px; -->
+<div style=""></div>
