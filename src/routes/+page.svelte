b11ab1d7bcab7378ddbd719338ad49dd0a18760c
Miel-DR2003 <mieldr1@gmail.com>
2024-09-09 11:33:38 +0200

    Add tailwind/postcss styling
    added the tailwind and postcss package and started some basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,4 +7,26 @@
 
 <!-- add css with tailwind -->
 <!-- margin:0 auto; padding: 20px; width:700px; -->
-<div style=""></div>
+<div class="mx-auto, p-5, w-[700px]">
+    <!-- text align center; -->
+    <h2 class="text-center, text-red-700">Todo list</h2>
+    <p>Enter new ToDo here</p>
+    <!-- display: flex; -->
+    <div class="flex">
+        <input type="text" />
+        <!-- width 200px; -->
+        <button class="w-52">Add</button>
+    </div>
+</div>
+
+<!-- display: flex; align-items: baseline; width: 700px; margin:0 auto; -->
+<div class="flex, align-baseline, w-[700], mx-auto">
+    <input type="checknox" />
+    <!-- flex grow:1; -->
+    <h4 class="flex-grow-1">Name of todo</h4>
+    <!-- Display: flex -->
+    <div class="flex">
+        <button>Edit</button>
+        <button>Delelte</button>
+    </div>
+</div>
